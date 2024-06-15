798ca21e6a87bfb06579ffa6d40fcca3df8c3bbc
unknown <maenurrokhman26@gmial.com>
2024-06-15 20:36:32 +0700

    layout

---
+++ app/src/main/java/com/submission/storyapp/view/custom/CustomEditText.kt
@@ -0,0 +1,85 @@
+package com.submission.storyapp.view.custom
+
+import androidx.appcompat.widget.AppCompatEditText
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.submission.storyapp.R
+
+class CustomEditText : AppCompatEditText {
+
+    private lateinit var lockIcon: Drawable
+    private var isPasswordVisible = false
+
+    constructor(context: Context) : super(context) {
+        initialize()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initialize()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initialize()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initialize() {
+        lockIcon = ContextCompat.getDrawable(context, R.drawable.ic_lock) as Drawable
+        compoundDrawablePadding = 12
+        setCompoundDrawablesWithIntrinsicBounds(null, null, lockIcon, null)
+        inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawableRight = 2
+                if (event.rawX >= (right - compoundDrawables[drawableRight].bounds.width())) {
+                    togglePasswordVisibility()
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                if (charSequence.isNullOrEmpty() || charSequence.length < 8) {
+                    error = context.getString(R.string.invalid_password)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(editable: Editable?) {}
+        })
+    }
+
+
+    private fun togglePasswordVisibility() {
+        isPasswordVisible = !isPasswordVisible
+        inputType = if (isPasswordVisible) {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+        setSelection(text!!.length)
+        setCompoundDrawablesWithIntrinsicBounds(null, null, lockIcon, null)
+    }
+
+}
